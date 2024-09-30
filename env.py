6858d9d5a89089f40b653c25989cd91ab561cf89
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-09-30 19:44:32 +0530

    Update env.py

--- env.py
+++ env.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-APP_ID= 10247139 
+API_ID= 10247139 
 API_HASH = "96b46175824223a33737657ab943fd6a"
 BOT_TOKEN= "5222572158:AAGwMiAMGgj9BmMQdcxn58Cq19stEnoVarI" 
 
